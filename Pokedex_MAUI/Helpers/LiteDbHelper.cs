e578813f6703160a6be818b0824ec6deed121b25
Angel David <angeldavidstudios@outlook.com>
2024-05-25 07:52:34 -0500

    Correcion de Modelos (Eliminados)

--- Pokedex_MAUI/Helpers/LiteDbHelper.cs
+++ Pokedex_MAUI/Helpers/LiteDbHelper.cs
@@ -1,28 +1,7 @@
+
 namespace Pokedex_MAUI.Helpers;
 
 public class LiteDbHelper
 {
-    public static void UpdateFiltersDataBase(LiteDbService<FiltersModel> db, FiltersModel filters)
-    {
-        db.DeleteAll();
-        db.UpsertItem(filters);
-    }
 
-    public static void UpdateResourceListDataBase(LiteDbService<ResourceListModel> db, ResourceListModel resourceList)
-    {
-        db.DeleteAll();
-        db.UpsertItem(resourceList);
-    }
-
-    public static void UpdatePokemonListDataBase(LiteDbService<PokemonModel> db, IEnumerable<PokemonModel> pokemons)
-    {
-        db.DeleteAll();
-        foreach (var item in pokemons)
-            db.UpsertItem(item);
-    }
-
-    public static void UpdatePokemonSpeciesDataBase(LiteDbService<PokemonSpeciesModel> db, PokemonSpeciesModel pokemonSpecie)
-    {
-        db.UpsertItem(pokemonSpecie);
-    }
 }
