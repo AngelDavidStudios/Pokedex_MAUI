db82ff8d448fe367fc837cbadf0bdc853e70a491
Angel David <angeldavidstudios@outlook.com>
2024-05-25 18:10:00 -0500

    Agregando colores y estilos por cada pokemon

--- Pokedex_MAUI/MauiProgram.cs
+++ Pokedex_MAUI/MauiProgram.cs
@@ -13,6 +13,9 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("sf-pro-display-regular.ttf", "FontRegular");
+                fonts.AddFont("sf-pro-display-medium.ttf", "FontMedium");
+                fonts.AddFont("sf-pro-display-bold.ttf", "FontBold");
             });
 
 #if DEBUG
