5079228be2cc6e3459380e0ae84f9fecf28d7b92
Angel David <angeldavidstudios@outlook.com>
2024-06-01 14:39:59 -0500

    Pruebas Exitosas en la ejecucion del Home con la lista de Pokemones

--- Pokedex_MAUI/MVVM/Views/HomeView.xaml.cs
+++ Pokedex_MAUI/MVVM/Views/HomeView.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Pokedex_MAUI.MVVM.ViewModels;
+using Pokedex_MAUI.Services;
 
 namespace Pokedex_MAUI.MVVM.Views;
 
@@ -11,5 +13,6 @@ public partial class HomeView : ContentPage
     public HomeView()
     {
         InitializeComponent();
+        BindingContext = new VMHome(Navigation, new RestService());
     }
 }
