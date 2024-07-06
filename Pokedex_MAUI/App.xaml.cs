2d97abc4a6490ba0ad65ddf614272ba400ffe3bd
Angel David <angeldavidstudios@outlook.com>
2024-07-06 13:00:29 -0500

    Configuracion de ejeucion Inicial AppShell

--- Pokedex_MAUI/App.xaml.cs
+++ Pokedex_MAUI/App.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using LiteDB;
+using Models.Login;
 using Pokedex_MAUI.MVVM.Views;
 
 namespace Pokedex_MAUI;
 
 public partial class App : Application
 {
+    public static Users User;
     public static LiteDatabase Database = new LiteDatabase(Path.Combine(FileSystem.AppDataDirectory, "pokedex.db"));
 
     public App()
---
+++ Pokedex_MAUI/MVVM/ViewModels/VMAppShell.cs
@@ -0,0 +1,17 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Pokedex_MAUI.MVVM.ViewModels;
+
+public partial class VMAppShell: ObservableObject
+{
+    [RelayCommand]
+    async void SignOut()
+    {
+        if (Preferences.ContainsKey(nameof(App.User)))
+        {
+            Preferences.Remove(nameof(App.User));
+        }
+        await Shell.Current.GoToAsync("//LoginPage");
+    }
+}
