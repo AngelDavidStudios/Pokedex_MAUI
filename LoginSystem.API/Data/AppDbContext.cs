4d8149f68c074bd70bf9a634c03bc536d235751c
Angel David <angeldavidstudios@outlook.com>
2024-07-06 12:39:50 -0500

    Creacion del AppDbCOntext Para la realizacion del EF a la Base de Datos SQL Azure

---
+++ LoginSystem.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Models.Login;
+
+namespace LoginSystem.API.Data;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Users> Users { get; set; } = default!;
+}
