65dba2ab0783dc2064f712863ec6fd0f303644db
Angel David <angeldavidstudios@outlook.com>
2024-07-06 13:20:28 -0500

    Creacion del ViewModel Par el Login y SignUp

--- Pokedex_MAUI/MVVM/ViewModels/VMLoginPage.cs
+++ Pokedex_MAUI/MVVM/ViewModels/VMLoginPage.cs
@@ -1,8 +1,54 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Models.Login;
+using Newtonsoft.Json;
+using Pokedex_MAUI.Services;
 
 namespace Pokedex_MAUI.MVVM.ViewModels;
 
-public class VMLoginPage: ObservableObject
+public partial class VMLoginPage: ObservableObject
 {
+    [ObservableProperty]
+    private string _userName;
+
+    [ObservableProperty]
+    private string _password;
+
+    readonly ILoginService loginService = new LoginService();
     
+    [RelayCommand]
+    public async void Login()
+    {
+        try {
+            if (Connectivity.Current.NetworkAccess == NetworkAccess.Internet) {
+
+                if (!string.IsNullOrWhiteSpace(UserName) && !string.IsNullOrWhiteSpace(Password)) {
+                    Users user = await loginService.Login(UserName, Password);
+                    if (user == null) {
+                        await Shell.Current.DisplayAlert("Error", "Username/Password is incorrect", "Ok");
+                        return;
+                    }
+                    if (Preferences.ContainsKey(nameof(App.User))) {
+                        Preferences.Remove(nameof(App.User));
+                    }
+                    string userDetails = JsonConvert.SerializeObject(user);
+                    Preferences.Set(nameof(App.User), userDetails);
+                    App.User = user;
+                    await Shell.Current.GoToAsync("//HomeView");
+                } else {
+                    await Shell.Current.DisplayAlert("Error", "All fields required", "Ok");
+                }
+            } else {
+                await Shell.Current.DisplayAlert("Error", "No Internet Access", "Ok");
+            }
+        } catch (Exception ex) {
+            await Shell.Current.DisplayAlert("Error", ex.Message, "Ok");
+        }
+    }
+    
+    [RelayCommand]
+    async Task ShowSignUpModal()
+    {
+        await Application.Current.MainPage.Navigation.PushModalAsync(new SignUpView());
+    }
 }
--- Pokedex_MAUI/MVVM/ViewModels/VMSignUp.cs
+++ Pokedex_MAUI/MVVM/ViewModels/VMSignUp.cs
@@ -1,6 +1,59 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Pokedex_MAUI.Services;
+
 namespace Pokedex_MAUI.MVVM.ViewModels;
 
-public class VMSignUp
+public partial class VMSignUp: ObservableObject
 {
+    [ObservableProperty]
+    private string fullName;
+
+    [ObservableProperty]
+    private string email;
+
+    [ObservableProperty]
+    private string password;
+
+    [ObservableProperty]
+    private string confirmPassword;
+
+    [ObservableProperty]
+    private string message;
+
+    private readonly ILoginService loginService;
     
+    public VMSignUp()
+    {
+        loginService = new LoginService();
+    }
+    
+    [RelayCommand]
+    public async Task Register()
+    {
+        if (string.IsNullOrWhiteSpace(FullName) || string.IsNullOrWhiteSpace(Email) || string.IsNullOrWhiteSpace(Password) || string.IsNullOrWhiteSpace(ConfirmPassword))
+        {
+            Message = "Please fill in all fields.";
+            return;
+        }
+
+        if (Password != ConfirmPassword)
+        {
+            Message = "Passwords do not match.";
+            return;
+        }
+        var success = await loginService.Register(FullName, Email, Password);
+        if (success != null)
+        {
+            Message = "Registration successful.";
+            await Application.Current.MainPage.DisplayAlert("Success", "Usuario Registrado con exito.", "Ok");
+            await Application.Current.MainPage.Navigation.PopModalAsync();
+
+        }
+        else
+        {
+            Message = "Registration failed.";
+            await Application.Current.MainPage.DisplayAlert("Error", "Error al registrar usuario.", "Ok");
+        }
+    }
 }
