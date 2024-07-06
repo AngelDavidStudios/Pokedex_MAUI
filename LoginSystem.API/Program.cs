888ed1b4fca96965e261b80f9f9606578c3e28de
Angel David <angeldavidstudios@outlook.com>
2024-07-06 12:41:13 -0500

    Program Realizado para la Pre Migracion

--- LoginSystem.API/Program.cs
+++ LoginSystem.API/Program.cs
@@ -1,10 +1,17 @@
+using LoginSystem.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection") ??
+                         throw new InvalidOperationException("Connection string 'Default Connection' not found'"));
+});
 
 var app = builder.Build();
 
